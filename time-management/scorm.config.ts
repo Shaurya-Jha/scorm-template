c1f4a6b5765541507972e683b94ac861f71e9b86
Shaurya-Jha <shauryajha1602@gmail.com>
2026-02-19 21:13:47 +0530

    updates

--- time-management/scorm.config.ts
+++ time-management/scorm.config.ts
@@ -2,7 +2,7 @@ export default {
   id: "course-tm-1",
   title: "Time Management",
   version: "1.0",
-  scorm: "2004",
+  scorm: "1.2",
   launch: "index.html",
   output: "course-time-management.zip"
 }
